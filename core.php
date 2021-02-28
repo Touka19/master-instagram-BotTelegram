bb5812b013520900562ac39d5bc35a54ed674114
Afshin Tavakolian | افشین توکلیان <afsh7n@gmail.com>
2021-03-01 00:56:25 +0330

    Add Function MassageRequestJson in file core.php

--- core.php
+++ core.php
@@ -1,3 +1,21 @@
 <?php
 define('BOT_TOKEN',"1556887896:AAFXcAgL1PYCclWyNDYKZ9Un8VmXsIGDsBY");
 define('API_URL',"https://api.telegram.org/bot".BOT_TOKEN.'/');
+
+function MassageRequestJson($method,$parameters){
+    
+    if (!$parameters){
+        $parameters = array();
+    }
+    
+    $parameters["method"] = $method;
+    
+    $handle = curl_init(API_URL);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($handle, CURLOPT_CONNECTTIMEOUT , 5);
+    curl_setopt($handle, CURLOPT_TIMEOUT , 60);
+    curl_setopt($handle, CURLOPT_POSTFIELDS , json_encode($parameters));
+    curl_setopt($handle, CURLOPT_HTTPHEADER , array("Content-Type: application/json"));
+    $result = curl_exec($handle);
+    return $result;
+}
