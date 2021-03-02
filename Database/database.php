43fafeb32302a6597398d6768aadc2febd639687
Afshin Tavakolian | افشین توکلیان <afsh7n@gmail.com>
2021-03-03 01:26:30 +0330

    complate method crud database && add 2 button in robot

--- Database/database.php
+++ Database/database.php
@@ -7,17 +7,66 @@ class database
     private $dbName;
     private $dbUser;
     private $dbPass;
+    private $nameTable;
     private $option;
-    public function __construct($dbname, $user, $pass)
+    public function __construct($dbname, $user, $pass,$nameTable)
     {
         $this->dbName = $dbname;
         $this->dbUser = $user;
         $this->dbPass = $pass;
+        $this->nameTable = $nameTable;
         $this->option = [PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,PDO::MYSQL_ATTR_INIT_COMMAND=>"SET NAMES utf8"];
         $this->dbConnect = new PDO("mysql:host=localhost;dbname=".$this->dbName,$this->dbUser,$this->dbPass,$this->option);
     }
 
+    public function ShowUser($chatID)
+    {
+        $result = $this->dbConnect->prepare("SELECT * FROM users WHERE chat_id=?");
+        $result->bindValue(1,$chatID);
+        $result->execute();
+        $row = $result->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    public function AddUser($chatID,$username,$firstname,$lang)
+    {
+
+        if($this->ShowUser($chatID) == false){
+            $result = $this->dbConnect->prepare("INSERT INTO users SET chat_id=?,username=?,firstname=?,lang=?,is_admin=?");
+            $result->bindValue(1,$chatID);
+            $result->bindValue(2,$username);
+            $result->bindValue(3,$firstname);
+            $result->bindValue(4,$lang);
+            $result->bindValue(5,0);
+            $result->execute();
+            return true;
+        }else{
+            return $this->ShowUser($chatID);
+        }
+    }
+
+    public function UpdateUser($chatID,$username,$firstname,$lang)
+    {
+        $result = $this->dbConnect->prepare("UPDATE users SET username=?,firstname=?,lang=?,is_admin=? WHERE chat_id=?");
+        $result->bindValue(1,$username);
+        $result->bindValue(2,$firstname);
+        $result->bindValue(3,$lang);
+        $result->bindValue(4,0);
+        $result->bindValue(5,$chatID);
+        $result->execute();
+        return true;
+    }
+
+    public function DeleteUser($chatID)
+    {
+        $result = $this->dbConnect->prepare("DELETE FROM users WHERE chat_id=?");
+        $result->bindValue(1,$chatID);
+        $result->execute();
+        return true;
+    }
+
+
+
 
 }
-
-$database = new database('master-instagram','root','');
+$dbUser = new database('master-instagram','root','','users');
--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <?php
 require_once './core/core.php';
+require_once './Database/database.php';
 
 $contentRequestTelegram = file_get_contents("php://input");
 $content = json_decode($contentRequestTelegram, true);
 $language = 'fa';
+
+//----------------------
+$dbUser = new database('master-instagram', 'root', '', 'users');
+
 
 if (isset($content["message"])) {
     $chat_id = $content["message"]['chat']['id'];
@@ -11,14 +16,74 @@ if (isset($content["message"])) {
     $message_id = $content["message"]["message_id"];
     $username = $content["message"]["chat"]['username'];
     $first_name = $content["message"]["chat"]['first_name'];
-} elseif (isset($update["callback_query"])) {
+} elseif (isset($content["callback_query"])) {
     $callback_id = $content["callback_query"]['id'];
     $chat_id = $content["callback_query"]["message"]['chat']['id'];
     $pv_id = $content["callback_query"]['from']['id'];
     $data = $content["callback_query"]["data"];
     $message_id = $content["callback_query"]["message"]['message_id'];
     $text = $content["callback_query"]["message"]["text"];
+    $username = $content["callback_query"]["message"]["chat"]['username'];
+    $first_name = $content["callback_query"]["message"]["chat"]['first_name'];
+}
+
+//----------User
+$user = $dbUser->ShowUser($chat_id);
+
+if ($text == "/start") {
+    MassageRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => "Please Select language 🇺🇸🇮🇷", 'reply_markup' => ['inline_keyboard' => [
+        [
+            ['text' => '🇮🇷فارسی🇮🇷', 'callback_data' => "!fa"]
+        ],
+        [
+            ['text' => '🇺🇸English🇺🇸', 'callback_data' => "!en"]
+        ]
+    ]]]);
+}
+
+if ($data == '!fa') {
+    $jsonFile = file_get_contents('language/fa.json');
+    $jsonLanguage = json_decode($jsonFile, true);
+
+} elseif ($data == '!en') {
+    $jsonFile = file_get_contents('language/en.json');
+    $jsonLanguage = json_decode($jsonFile, true);
+}
+
+//-------------------
+switch ($data) {
+    case "!fa":
+        MassageRequestJson('editMessageText', ['chat_id' => $chat_id, 'message_id' => $message_id, 'text' => $jsonLanguage['welcome'], 'reply_markup' => ['inline_keyboard' => [
+            [
+                ['text' => $jsonLanguage['information'], 'callback_data' => "!information"]
+            ],
+            [
+                ['text' => $jsonLanguage['about'], 'callback_data' => "!about"]
+            ]
+        ]]]);
+        $tr =$dbUser->AddUser($chat_id, $username, $first_name, 'fa');
+        break;
+    case '!information':
+        MassageRequestJson('editMessageText', ['chat_id' => $chat_id, 'message_id' => $message_id, 'text' => $jsonLanguage['getInformation'], 'reply_markup' => ['inline_keyboard' => [
+            [
+                ['text' => $jsonLanguage['information'], 'callback_data' => "!information"]
+            ],
+            [
+                ['text' => $jsonLanguage['about'], 'callback_data' => "!about"]
+            ]
+        ]]]);
+        break;
+    case '!about':
+        MassageRequestJson('editMessageText', ['chat_id' => $chat_id, 'message_id' => $message_id, 'text' => $jsonLanguage['about'], 'reply_markup' => ['inline_keyboard' => [
+            [
+                ['text' => $jsonLanguage['information'], 'callback_data' => "!information"]
+            ],
+            [
+                ['text' => $jsonLanguage['about'], 'callback_data' => "!about"]
+            ]
+        ]]]);
+        break;
+
 }
 
 
-MassageRequestJson('sendMessage', ['chat_id' => $chat_id, 'text' => $text]);
